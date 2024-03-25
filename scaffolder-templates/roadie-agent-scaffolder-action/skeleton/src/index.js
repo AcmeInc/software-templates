f01b48fe231ce36ba1f35192de07ad08da6837e6
Brian Fletcher <brian.jf.fletcher@gmail.com>
2024-03-25 10:29:05 +0000

    Update index.js

--- scaffolder-templates/roadie-agent-scaffolder-action/skeleton/src/index.js
+++ scaffolder-templates/roadie-agent-scaffolder-action/skeleton/src/index.js
@@ -2,12 +2,12 @@ import { RoadieAgent, createRoadieAgentScaffolderAction } from '@roadiehq/roadie
 import { myScaffolderActionHandler } from './myScaffolderActionHandler.js';
 
 RoadieAgent.fromConfig({
-    server: 'https://${{ roadie.tenantName }}.broker.roadie.so',
+    server: 'https://${{ values.tenant }}.broker.roadie.so',
     accept: `${process.cwd()}/config/accept.json`,
     identifier: process.env.BROKER_TOKEN
   })
   .addScaffolderAction(createRoadieAgentScaffolderAction({
-    name: '${{ roadie.tenantName }}',
+    name: '${{ values.tenant }}',
     handler: myScaffolderActionHandler
   }))
   .start();
